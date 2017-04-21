62a809d9197cfb3f7f780f37d0b631f6e7bb7610
Danish Khan <s236604@stud.hioa.no>
2017-04-21 23:12:03 +0200

    changes 21

---
+++ Studentparlamentet-28/Model/Preferansevalg.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studentparlamentet_28.Models
+{
+    public class Preferansevalg
+    {
+        public int id { get; set; }
+
+
+        public string beskrivelse { get; set; }
+        public int antallrepresentanter { get; set; }
+        public bool utført { get; set; }
+        public int valgtypeid { get; set; }
+    }
+}
---
+++ Studentparlamentet-28/Model/Stemmeseddel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studentparlamentet_28.Models
+{
+    public class Stemmeseddel
+    {
+        public int stemmeseddelID { get; set; }
+        public string kandidatnrEn { get; set; }
+        public string kandidatnrTo { get; set; }
+        public string kandidatnrTre { get; set; }
+        public string kandidatnrFire { get; set; }
+        public string kandidatnrFem { get; set; }
+        public string kandidatnrSeks { get; set; }
+        public string kandidatnrSju { get; set; }
+        public string kandidatnrÅtte { get; set; }
+        public string kandidatnrNi { get; set; }
+        public string kandidatnrTi { get; set; }
+        public string kandidatnrElleve { get; set; }
+        public string kandidatnrTolv { get; set; }
+
+        public int valgtypeid { get; set; }
+    }
+}
